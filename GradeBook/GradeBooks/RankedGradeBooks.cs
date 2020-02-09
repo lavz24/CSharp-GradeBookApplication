f1f707418bd45bcc3d1ec99a8944217df3761866
Luis Alejandro Vieira <lavz24@gmail.com>
2020-02-09 19:26:06 +0000

    Creating New GradeBookTypes

---
+++ GradeBook/GradeBooks/RankedGradeBooks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBooks : BaseGradeBook
+    {
+        public RankedGradeBooks(string name) : base(name)
+        {
+            this.Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
